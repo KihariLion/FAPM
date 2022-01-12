656aa6aa485aa03fddc0e69efdb43624e92d5ae0
Kihari <92060738+KihariLion@users.noreply.github.com>
2022-01-11 17:00:51 -0800

    Bump to 2022.1.11.1

--- fapm/__main__.py
+++ fapm/__main__.py
@@ -15,7 +15,7 @@ from sqlalchemy.types import Integer, Unicode
 from werkzeug.utils import secure_filename
 
 
-VERSION = '2022.1.11.0'
+VERSION = '2022.1.11.1'
 
 HELP = f"""
 FurAffinity Private Message Downloader
